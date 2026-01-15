391bcdbbc4c6e08de7122c47b2500572bdfc0323
Moises Alonso <digiprodsgt@gmail.com>
2026-01-14 20:42:43 -0600

    Add new class file Archivo.java

---
+++ Archivo.java
@@ -0,0 +1,44 @@
+public class Archivo {
+    private String nombre;
+    private String extension;
+    private long tamaño; // en bytes
+
+    public Archivo(String nombre, String extension, long tamaño) {
+        this.nombre = nombre;
+        this.extension = extension;
+        this.tamaño = tamaño;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public long getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(long tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    @Override
+    public String toString() {
+        return "Archivo{" +
+                "nombre='" + nombre + '\'' +
+                ", extension='" + extension + '\'' +
+                ", tamaño=" + tamaño +
+                '}';
+    }
+}
